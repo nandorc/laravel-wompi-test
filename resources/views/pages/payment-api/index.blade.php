77afb4e99b9f0763175dfddcb10235db8ab6ab1c
Daniel F. Rivera C <daniel.rivera@pragma.com.co>
2021-10-13 23:43:46 -0500

    Configure main navigation and breadcrumb

---
+++ resources/views/pages/payment-api/index.blade.php
@@ -0,0 +1,5 @@
+@php
+$path = ['payment-api.index' => 'API de Pagos'];
+@endphp
+@extends('templates.layout-breadcrumb',['path'=>$path])
+@section('head__title', 'API de Pagos')
---
+++ resources/views/pages/plugins/index.blade.php
@@ -0,0 +1,5 @@
+@php
+$path = ['plugins.index' => 'Plugins E-commerce'];
+@endphp
+@extends('templates.layout-breadcrumb',['path'=>$path])
+@section('head__title', 'Plugins E-commerce')
