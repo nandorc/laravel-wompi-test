2c63394ad2d3dd06b3fe4b58e7596c971b3dce7a
Daniel F. Rivera C <daniel.rivera@pragma.com.co>
2021-10-18 18:40:08 -0500

    Create reoute and controller to handle wompi events

---
+++ app/Http/Controllers/Wompi/EventsController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Wompi;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class EventsController extends Controller
+{
+    public function handle(Request $request)
+    {
+
+    }
+}
